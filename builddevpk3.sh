61294989f050e44156fa39fd7abbe9319faf92d4
NeonKnightOA <openarena.arg@gmail.com>
2026-07-21 21:09:31 -0300

    Updated build scripts

--- builddevpk3.sh
+++ builddevpk3.sh
@@ -4,18 +4,20 @@
 #this script will not work.  Please check your distro's
 #documentation on how to install the zip package.
 now=$(date +"%Y_%m_%d")
+pakversion=088b
+pakfilename=pak6-patch
 
 echo Compressing...
-./zip -9r pak6-patch088b-${now}-temp.pk3 docs/*
-./zip -9r pak6-patch088b-${now}-temp.pk3 levelshots/*
-./zip -9r pak6-patch088b-${now}-temp.pk3 maps/*
-./zip -9r pak6-patch088b-${now}-temp.pk3 models/*
-./zip -9r pak6-patch088b-${now}-temp.pk3 music/*
-./zip -9r pak6-patch088b-${now}-temp.pk3 scripts/*
-./zip -9r pak6-patch088b-${now}-temp.pk3 sources/*
-#./zip -9r pak6-patch088b-${now}-temp.pk3 sound/*
-./zip -9r pak6-patch088b-${now}-temp.pk3 textures/*
-mv pak6-patch088b-${now}-temp.pk3 pak6-patch088b-${now}.pk3
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 docs/*
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 levelshots/*
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 maps/*
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 models/*
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 music/*
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 scripts/*
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 sources/*
+#./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 sound/*
+./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 textures/*
+mv ${pakfilename}-v${pakversion}-${now}-temp.pk3 ${pakfilename}-v${pakversion}-${now}.pk3
 echo Done
 
 exit 0
