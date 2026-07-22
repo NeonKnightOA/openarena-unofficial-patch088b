391f20a3d784420ce3c8e2db03fc638ce188ce9e
NeonKnightOA <openarena.arg@gmail.com>
2026-07-22 03:04:20 -0300

    Build scripts now generate pak7-patch*

--- builddevpk3.bat
+++ builddevpk3.bat
@@ -7,20 +7,20 @@ rem documentation on how to install the zip package.
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "pakversion=088b"
-set "pakfilename=pak6-patch"
+set "pakfilename=pak7-patch"
 
 set "now=%YYYY%_%MM%_%DD%"
 
 echo Compressing...
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 docs/*
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 levelshots/*
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 maps/*
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 models/*
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 music/*
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 scripts/*
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 sources/*
-rem zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 sound/*
-zip.exe -9r %pakfilename%-v%pakversion%-%now%-temp.pk3 textures/*
-ren %pakfilename%-v%pakversion%-%now%-temp.pk3 %pakfilename%-v%pakversion%-%now%.pk3
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 docs/*
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 levelshots/*
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 maps/*
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 models/*
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 music/*
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 scripts/*
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 sources/*
+rem zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 sound/*
+zip.exe -9r %pakfilename%%pakversion%-%now%-temp.pk3 textures/*
+ren %pakfilename%%pakversion%-%now%-temp.pk3 %pakfilename%%pakversion%-%now%.pk3
 echo Done
 @echo on
--- builddevpk3.sh
+++ builddevpk3.sh
@@ -5,19 +5,19 @@
 #documentation on how to install the zip package.
 now=$(date +"%Y_%m_%d")
 pakversion=088b
-pakfilename=pak6-patch
+pakfilename=pak7-patch
 
 echo Compressing...
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 docs/*
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 levelshots/*
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 maps/*
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 models/*
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 music/*
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 scripts/*
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 sources/*
-#./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 sound/*
-./zip -9r ${pakfilename}-v${pakversion}-${now}-temp.pk3 textures/*
-mv ${pakfilename}-v${pakversion}-${now}-temp.pk3 ${pakfilename}-v${pakversion}-${now}.pk3
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 docs/*
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 levelshots/*
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 maps/*
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 models/*
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 music/*
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 scripts/*
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 sources/*
+#./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 sound/*
+./zip -9r ${pakfilename}${pakversion}-${now}-temp.pk3 textures/*
+mv ${pakfilename}${pakversion}-${now}-temp.pk3 ${pakfilename}${pakversion}-${now}.pk3
 echo Done
 
 exit 0
